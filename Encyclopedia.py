2da3dedfad866b01e96f427b1e1287f46ed2a658
Hiro Hamada <laciferin@gmail.com>
2020-03-28 22:12:24 +0530

    Update Encyclopedia.py

--- Encyclopedia.py
+++ Encyclopedia.py
@@ -2960,3 +2960,45 @@ def sort(l1,l2):
 print(*sort(l1,l2))        
 
 
+## 28-3
+## Merge-Sort
+
+n =int(input())
+arr=list(map(int,input().split()))
+
+
+
+def mergeSort(arr): 
+    if len(arr) >1: 
+        mid = len(arr)//2 
+        L = arr[:mid]
+        R = arr[mid:] 
+        mergeSort(L) 
+        mergeSort(R) 
+         
+        i = j = k = 0
+          
+        while i < len(L) and j < len(R): 
+            if L[i] < R[j]: 
+                arr[k] = L[i]
+                i+=1
+            else: 
+                arr[k] = R[j] 
+                j+=1
+            k+=1
+          
+        while i < len(L): 
+            arr[k] = L[i] 
+            i+=1
+            k+=1
+          
+        while j < len(R): 
+            arr[k] = R[j] 
+            j+=1
+            k+=1
+
+mergeSort(arr)
+
+print(*arr)
+
+
